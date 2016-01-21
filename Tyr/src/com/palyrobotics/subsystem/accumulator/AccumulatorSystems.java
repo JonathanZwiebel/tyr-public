2abbf6e30ab336a8db770d9095a5df66a7337a60
Nihar Mitra <nihar.roy.mitra@gmail.com>
2016-01-20 17:07:17 -0800

    Fixed name of AccumulatorSystems

--- Tyr/src/com/palyrobotics/subsystem/accumulator/AccumulatorSystems.java
+++ Tyr/src/com/palyrobotics/subsystem/accumulator/AccumulatorSystems.java
@@ -3,7 +3,7 @@ package com.palyrobotics.subsystem.accumulator;
 import org.strongback.components.Motor;
 import org.strongback.hardware.Hardware;
 
-public class AccumulatorSytems {
+public class AccumulatorSystems {
 	private Motor leftMotor = Hardware.Motors.talon(6);
 	private Motor rightMotor = Hardware.Motors.talon(7);
 }
