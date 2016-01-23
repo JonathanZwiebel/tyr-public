8da49b105e7b6a8b1ea2aa3fa606c510f421c383
Jonathan Zwiebel <jonathanzwiebel@gmail.com>
2016-01-23 13:55:50 -0800

    Updated shooter hardware to act as an interface

--- Tyr/src/com/palyrobotics/subsystem/shooter/ShooterHardware.java
+++ Tyr/src/com/palyrobotics/subsystem/shooter/ShooterHardware.java
@@ -3,16 +3,16 @@ package com.palyrobotics.subsystem.shooter;
 import org.strongback.components.Motor;
 import org.strongback.hardware.Hardware;
 
-public class ShooterHardware extends ShooterSystems {
+public class ShooterHardware implements ShooterSystems {
 	Motor motor = Hardware.Motors.talon(-1);
 
 	@Override
-	protected Motor getMotor() {
+	public Motor getMotor() {
 		return motor;
 	}
 
 	@Override
-	protected void setMotor(Motor motor) {
+	public void setMotor(Motor motor) {
 		this.motor = motor;
 	}
 }
--- Tyr/src/com/palyrobotics/subsystem/shooter/ShooterSystems.java
+++ Tyr/src/com/palyrobotics/subsystem/shooter/ShooterSystems.java
@@ -2,8 +2,8 @@ package com.palyrobotics.subsystem.shooter;
 
 import org.strongback.components.Motor;
 
-public abstract class ShooterSystems {	
-	protected abstract Motor getMotor();
-	protected abstract void setMotor(Motor motor);
+public interface ShooterSystems {	
+	public Motor getMotor();
+	public void setMotor(Motor motor);
 	// this will be either a talon (ShooterHardware) or a mock talon (MockShooterHardware)
 }
