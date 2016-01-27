21cd2b2204f169af601461acdbace6e844a47804
prashantiA <psunander@gmail.com>
2016-01-26 18:04:32 -0800

    Changed the name of StartAccumulatorControl to AccumulatorTeleopControl

--- Tyr/src/com/palyrobotics/subsystem/accumulator/AccumulatorTeleop.java
+++ Tyr/src/com/palyrobotics/subsystem/accumulator/AccumulatorTeleop.java
@@ -3,37 +3,38 @@ package com.palyrobotics.subsystem.accumulator;
 import org.strongback.Strongback;
 import org.strongback.SwitchReactor;
 import org.strongback.command.Command;
+import org.strongback.components.ui.FlightStick;
 
 import com.palyrobotics.robot.InputSystems;
 
 import static com.palyrobotics.subsystem.accumulator.AccumulatorConstants.*;
 
-public class AccumulatorTeleop extends Command {
+//TODO: perhaps rename class to teleopControl or something
+public class AccumulatorTeleop extends Command{
 	AccumulatorController controller;
 	SwitchReactor reactor;
 	InputSystems input;
-
-	public AccumulatorTeleop(AccumulatorController controller, InputSystems input) {
-		// Constructs the command using the super constructor
-		super(controller);
+	
+	public AccumulatorTeleop (AccumulatorController controller, InputSystems input) {
+		//Constructs the command using the super constructor
+		super (controller);
 		this.controller = controller;
-		this.input = input;
 		reactor = Strongback.switchReactor();
+		this.input = input;
 	}
-
-	/**
-	 * Submits commands to Strongback based on button input TODO: Turn switch
-	 * reactors off when teleop is left
-	 */
 	@Override
 	public boolean execute() {
-		// Accumulates the ball if the button is pressed
-		reactor.onTriggered(input.getOperatorStick().getButton(ACCUMULATE_BUTTON), () -> Strongback.submit(new IntakeBall(controller, input)));
-
-		// Expels the ball when the expel button is pressed
-		reactor.onTriggered(input.getOperatorStick().getButton(EXPEL_BUTTON), () -> Strongback.submit(new ExpelBall(controller)));
-		reactor.onTriggered(input.getOperatorStick().getButton(STOP_BUTTON), () -> Strongback.submit(new StopAccumulator(controller)));
-		return true;
+		//Accumulates the ball if the button is pressed
+		reactor.onTriggered(input.getOperatorStick().getButton(ACCUMULATE_BUTTON),()->Strongback.submit(new IntakeBall(controller, input)));
+		
+		//Expels the ball when the expel button is pressed
+		reactor.onTriggered(input.getOperatorStick().getButton(EXPEL_BUTTON),()->Strongback.submit(new ExpelBall(controller)));
+				
+		//TODO: whenever the expel button is not pressed, even if the accumulate button is pressed it will not do much/anything, as stop is last
+		//TODO: and as such overrides the others. perhaps look into reworking button press scheme.
+		//TODO: also this command never finishes, it always returns false. it does end when another command is called that requires controller however.
+		
+		return false;
 	}
 
 }
