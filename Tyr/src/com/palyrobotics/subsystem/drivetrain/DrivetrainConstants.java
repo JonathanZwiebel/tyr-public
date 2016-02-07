e50a0186460fc2cb7c234a89dfa668e0a3b1d1ff
Bert Davies <bertdaviezz@gmail.com>
2016-02-06 17:03:01 -0800

    Implemented basic PD angle turning

--- Tyr/src/com/palyrobotics/subsystem/drivetrain/DrivetrainConstants.java
+++ Tyr/src/com/palyrobotics/subsystem/drivetrain/DrivetrainConstants.java
@@ -9,9 +9,18 @@ public class DrivetrainConstants {
 	public static final double ENCODER_PULSES_PER_DEGREE = 0.0;
 	
 	public static final double RIGHT_P_VALUE = 0.09;
-	public static final double LEFT_P_VALUE = 0.1;
+	public static final double LEFT_P_VALUE = -0.1;
 	public static final double RIGHT_D_VALUE = 0.003;
-	public static final double LEFT_D_VALUE = 0.003;
-	public static final double UPDATE_RATE = 0.02;
+	public static final double LEFT_D_VALUE = -0.003;
+	
+	public static final double RIGHT_ANGLE_P_VALUE = 0.09;
+	public static final double LEFT_ANGLE_P_VALUE = 0.1;
+	public static final double RIGHT_ANGLE_D_VALUE = 0.003;
+	public static final double LEFT_ANGLE_D_VALUE = 0.003;
+	
+	public static final double ANGLE_TO_DISTANCE = 0.25;
+	public static final double UPDATE_RATE = 0.02; //50Hz update rate
+	public static final double ACCEPTABLE_ANGLE_ERROR = 3.0;
 	public static final double ACCEPTABLE_DISTANCE_ERROR = 1.0;
+
 }
--- Tyr/src/com/palyrobotics/subsystem/drivetrain/TurnAngle.java
+++ Tyr/src/com/palyrobotics/subsystem/drivetrain/TurnAngle.java
@@ -3,19 +3,34 @@ package com.palyrobotics.subsystem.drivetrain;
 import org.strongback.command.Command;
 
 import com.palyrobotics.subsystem.drivetrain.DrivetrainController.State;
+import static com.palyrobotics.subsystem.drivetrain.DrivetrainConstants.*;
 
 public class TurnAngle extends Command {
 
 	private DrivetrainController drivetrain;
 	private double angle;
+	private double previousError;
 	
+	/**
+	 * Starts the command running, passing the drivetrain for input and
+	 * output as well as the desired angle to turn to.
+	 * @param drivetrain
+	 * @param angle
+	 */
 	public TurnAngle(DrivetrainController drivetrain, double angle) {
 		super(drivetrain.getRequirements());
 		this.drivetrain = drivetrain;
 		this.angle = angle;
+		this.previousError = angle;
 	}
+	
 	/**
-	 * Zero encoders and set state to turning angle
+<<<<<<< HEAD
+	 * Called at the beginning of the command, and sets state to turning as well as zeros the encoders.	
+=======
+	 * Called at the beginning of the command, and sets state to turning.
+	 * Zeros encoders.
+>>>>>>> da9ef1b... Zeroed encoders.
 	 */
 	@Override
 	public void initialize() {
@@ -24,9 +39,57 @@ public class TurnAngle extends Command {
 		drivetrain.input.getRightDriveEncoder().zero();
 	}
 	
+	/**
+	 * Executes 200 times a second, as long as the command is running.
+	 * PID values are computed and the speed of the composed motors are set.
+	 * Returns true when completed, false to run again.
+	 */
 	@Override
 	public boolean execute() {
+		//calculate error by using angle to distance 
+		double error =  angle - drivetrain.input.getGyroscope().getAngle();
 		
-		return true;
+		//finds derivative with 1/200 update rate
+		double derivative = (error - previousError) / UPDATE_RATE; 
+		
+		previousError = error;
+		
+		//min and max restrict the speeds between -0.5 and 0.5
+		double leftSpeed = Math.max(Math.min(LEFT_ANGLE_P_VALUE * error + LEFT_ANGLE_D_VALUE * derivative, 0.5), -0.5); 
+		double rightSpeed = Math.max(Math.min(RIGHT_ANGLE_P_VALUE * error + RIGHT_ANGLE_D_VALUE * derivative, 0.5), -0.5);
+		
+		drivetrain.output.getLeftMotor().setSpeed(leftSpeed);
+		drivetrain.output.getRightMotor().setSpeed(rightSpeed);
+		
+		//stops robot when target is reached and robot has slowed within tolerance range
+		if(derivative == 0.0 && Math.abs(error) < ACCEPTABLE_ANGLE_ERROR) { 
+			drivetrain.output.getLeftMotor().setSpeed(0.0);
+			drivetrain.output.getRightMotor().setSpeed(0.0);
+			drivetrain.setState(State.IDLE);
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Called when command is interrupted. 
+	 * Stops the motors and sets the State to idle.
+	 */
+	@Override
+	public void interrupted() {
+		drivetrain.output.getLeftMotor().setSpeed(0.0);
+		drivetrain.output.getRightMotor().setSpeed(0.0);
+		drivetrain.setState(State.IDLE);
+	}
+	
+	/**
+	 * Called when the command has finished. 
+	 * Stops the motors and sets the State to idle.
+	 */
+	@Override
+	public void end() {
+		drivetrain.output.getLeftMotor().setSpeed(0.0);
+		drivetrain.output.getRightMotor().setSpeed(0.0);
+		drivetrain.setState(State.IDLE);
 	}
 }
