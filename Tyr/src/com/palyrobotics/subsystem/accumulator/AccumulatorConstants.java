b68a45b4abef822dc1a28a6c5307c55889e1c38b
Eric Liu <ericxliu@gmail.com>
2016-02-18 15:42:22 -0800

    finished fixing accumulator

--- Tyr/src/com/palyrobotics/subsystem/accumulator/AccumulatorConstants.java
+++ Tyr/src/com/palyrobotics/subsystem/accumulator/AccumulatorConstants.java
@@ -1,16 +1,14 @@
 package com.palyrobotics.subsystem.accumulator;
 
 public class AccumulatorConstants {
-	private static final Double DOUBLE = (Double) null;
-	private static final Integer INTEGER = (Integer) null;
 
 	//Speed at which the accumulator is run
-	public static final double ACCUMULATOR_POWER = -0.75f;
+	public static final double ACCUMULATOR_POWER = 1;
 	//Buttons for intaking and expelling balls
-	public static final int ACCUMULATE_BUTTON  = INTEGER;
-	public static final int EXPEL_BUTTON = INTEGER;
-	public static final int STOP_BUTTON = INTEGER;
+	public static final int ACCUMULATE_BUTTON  = 3;
+	public static final int EXPEL_BUTTON = 4;
+	public static final int STOP_BUTTON = 6;
 	
 	//TODO: Period of time the accumulator will keep expelling
-	public static final double EXPEL_TIME = DOUBLE;
+	public static final double EXPEL_TIME = 3000;
 }
