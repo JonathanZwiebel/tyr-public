f59976cd89f4751db44c3cdbbb6817c821568653
Driver Station <contact@palyrobotics.com>
2016-03-11 16:39:44 -0800

    Changed grabber values

--- Tyr/src/com/palyrobotics/subsystem/grabber/GrabberConstants.java
+++ Tyr/src/com/palyrobotics/subsystem/grabber/GrabberConstants.java
@@ -2,5 +2,5 @@ package com.palyrobotics.subsystem.grabber;
 
 public class GrabberConstants {	
 	public static final float UPPER_POSITION_VOLTAGE = 0.8f;
-	public static final float LOWER_POSITION_VOLTAGE = 0.2f;
+	public static final float LOWER_POSITION_VOLTAGE = 0.6f;
 }
