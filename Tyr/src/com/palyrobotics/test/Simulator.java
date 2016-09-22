e22dfcd48e28d6c1c36171c6b3fa88a8ab5bf825
Benjamin Cohen-Wang <ben.cohenwang@gmail.com>
2016-09-22 00:24:10 -0700

    BCW-20160921-2 Implemented skeleton for simulation builder

--- Tyr/src/com/palyrobotics/test/Simulator.java
+++ Tyr/src/com/palyrobotics/test/Simulator.java
@@ -1,23 +1,22 @@
 package com.palyrobotics.test;
 
-import com.palyrobotics.robot.RobotController;
 import com.palyrobotics.test.SimulatorConstants;
 import com.palyrobotics.test.simulations.Simulation;
 
 public class Simulator {
 	
-	private RobotController robot;
+	public static void main(String args[]) {
+		
+	}
 	
-	// TODO: Implement system for inputs
-	public Simulator() {
-		this.robot = new RobotController();
+	private Simulation simulation;
+
+	public Simulator(Simulation simulation) {
+		this.simulation = simulation;
 	}
 	
 	// TODO: Implement the actual WPI state machine at http://www.frc.ri.cmu.edu/project/frcsoftware/wordpress/?p=32
-	public void simulate(Simulation simulation) {
-		// TODO: Come up with a more elegant way to do this
-		simulation.setRobot(robot);
-		
+	public void simulate(Simulation simulation) {		
 		int updates = 0;
 		double time = 0.0;
 		
