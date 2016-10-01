7f45633b65b56270a5f344243add62da458ccdab
Jonathan Zwiebel <jonathanzwiebel@gmail.com>
2016-10-01 14:49:25 -0700

    JZ100116-2 Updated encoder constants for both Tyr and Deric

--- Tyr/src/com/palyrobotics/robot/SensorConstants.java
+++ Tyr/src/com/palyrobotics/robot/SensorConstants.java
@@ -13,14 +13,15 @@ public class SensorConstants {
 	public static final float BREACHER_POTENTIOMETER_DEGREE_OFFSET_TYR = 1.0f;
 	public static final float SHOOTER_POTENTIOMETER_FULL_VOLTAGE_RANGE_TO_DEGREES_TYR = 1.0f;
 	public static final float SHOOTER_POTENTIOMETER_DEGREE_OFFSET_TYR = 1.0f;
-	public static final float LEFT_DRIVETRAIN_DPP_TYR = 1.0f;
-	public static final float RIGHT_DRIVETRAIN_DPP_TYR = 1.0f;
 	
 	public static final float BREACHER_POTENTIOMETER_FULL_VOLTAGE_RANGE_TO_DEGREES_DERIC = 3600;
 	public static final float BREACHER_POTENTIOMETER_DEGREE_OFFSET_DERIC = -3425;
 	public static final float SHOOTER_POTENTIOMETER_FULL_VOLTAGE_RANGE_TO_DEGREES_DERIC = 2400;
 	public static final float SHOOTER_POTENTIOMETER_DEGREE_OFFSET_DERIC = -175;
 
-	public static final double LEFT_DRIVETRAIN_DPP_DERIC = -0.183621;
-	public static final double RIGHT_DRIVETRAIN_DPP_DERIC = -.185684;
+	// Mean of measured values on Tyr
+	public static final float LEFT_DRIVETRAIN_DPP_TYR = 0.184f;
+	public static final float RIGHT_DRIVETRAIN_DPP_TYR = -0.184f;
+	public static final double LEFT_DRIVETRAIN_DPP_DERIC = -0.184f;
+	public static final double RIGHT_DRIVETRAIN_DPP_DERIC = -0.184f;
 }
