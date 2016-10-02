cb0bca3d89a8ebc5a3b322d8ea387973448ddda0
Driver Station <nmitra@palyrobotics.com>
2016-10-02 13:39:39 -0700

    Working operator binds.

--- Tyr/src/com/palyrobotics/xbox/Converter.java
+++ Tyr/src/com/palyrobotics/xbox/Converter.java
@@ -63,10 +63,10 @@ public class Converter {
 		leftStick.setButton(LOADING_ACTUATOR_RETRACT_OPERATOR_STICK_BUTTON, xbox.getX().isTriggered());
 		
 		//Maps the right trigger to the firing button of the left joystick
-		leftStick.setButton(LOCKING_ACTUATOR_UNLOCK_OPERATOR_STICK_BUTTON, xbox.getY().isTriggered());
+		leftStick.setButton(LOCKING_ACTUATOR_UNLOCK_OPERATOR_STICK_BUTTON, xbox.getA().isTriggered());
 		
 		//Maps the right bumper to the unlock actuator button of the left joystick
-		leftStick.setButton(LOCKING_ACTUATOR_LOCK_OPERATOR_STICK_BUTTON, xbox.getA().isTriggered());
+		leftStick.setButton(LOCKING_ACTUATOR_LOCK_OPERATOR_STICK_BUTTON, xbox.getY().isTriggered());
 				
 		//Maps the left bumper to the grabber toggle button of the right joystick
 		rightStick.setButton(GRABBER_TOGGLE_BUTTON, xbox.getLeftBumper().isTriggered());
