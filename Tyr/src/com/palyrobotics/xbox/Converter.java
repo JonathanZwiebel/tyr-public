87e459bfc0daf9c39ba7cd1e20c1b3ce6748f7e5
Bert Davies <bertdaviezz@gmail.com>
2016-10-01 21:10:51 -0700

    Modified operator binds.

--- Tyr/src/com/palyrobotics/xbox/Converter.java
+++ Tyr/src/com/palyrobotics/xbox/Converter.java
@@ -7,7 +7,7 @@ public class Converter {
 	/**
 	 * 
 	 * @param xbox the xbox
-	 * @param leftStick shooter
+	 * @param leftStick expeler
 	 * @param rightStick secondary
 	 */
 	public static void convert(XBoxController xbox, MockFlightStick leftStick, MockFlightStick rightStick) {
@@ -18,35 +18,35 @@ public class Converter {
 		 * Left XBox Joystick -> leftStick
 		 * Right XBox Joystick -> rightStick
 		 * Right XBox Joystick Button -> breacher hold
-		 * Left XBox Joystick Button -> shooter hold
+		 * Left XBox Joystick Button -> expeler hold
 		 * Left Bumper -> toggle grabber
 		 * Right Bumper -> close lock
-		 * Left Trigger -> accumulate
-		 * Right Trigger -> shoot
-		 * A -> shooter extend
-		 * B -> shooter retract
+		 * Left Trigger -> intake
+		 * Right Trigger -> expel
+		 * A -> expeler extend
+		 * B -> expeler retract
 		 * X -> expel ball
 		 * Y -> stop accumulator
 		 */
 		
-		//Controls the shooting
-		boolean shoot;
+		//Controls the expeling
+		boolean expel;
 		
 		//Controls the accumulating
-		boolean accumulate;
+		boolean intake;
 		
 		if(xbox.getRightTrigger().read() > 0.9) {
-			shoot = true;
+			expel = true;
 		}
 		else {
-			shoot = false;
+			expel = false;
 		}
 		
 		if(xbox.getLeftTrigger().read() > 0.9) {
-			accumulate = true;
+			intake = true;
 		}
 		else {
-			accumulate = false;
+			intake = false;
 		}
 		
 		//Maps the left stick of the xbox to the left joystick
@@ -57,30 +57,27 @@ public class Converter {
 		leftStick.setYaw(xbox.getLeftTrigger().read());
 		
 		//Maps a to the extend actuator button of the left joystick
-		leftStick.setButton(LOADING_ACTUATOR_EXTEND_OPERATOR_STICK_BUTTON, xbox.getA().isTriggered());
+		leftStick.setButton(LOADING_ACTUATOR_EXTEND_OPERATOR_STICK_BUTTON, xbox.getB().isTriggered());
 		
 		//Maps b to the retract actuator button of the left joystick 
-		leftStick.setButton(LOADING_ACTUATOR_RETRACT_OPERATOR_STICK_BUTTON, xbox.getB().isTriggered());
+		leftStick.setButton(LOADING_ACTUATOR_RETRACT_OPERATOR_STICK_BUTTON, xbox.getX().isTriggered());
 		
 		//Maps the right trigger to the firing button of the left joystick
-		leftStick.setButton(LOCKING_ACTUATOR_UNLOCK_OPERATOR_STICK_BUTTON, shoot);
+		leftStick.setButton(LOCKING_ACTUATOR_UNLOCK_OPERATOR_STICK_BUTTON, xbox.getY().isTriggered());
 		
 		//Maps the right bumper to the unlock actuator button of the left joystick
-		leftStick.setButton(LOCKING_ACTUATOR_LOCK_OPERATOR_STICK_BUTTON, xbox.getRightBumper().isTriggered());
+		leftStick.setButton(LOCKING_ACTUATOR_LOCK_OPERATOR_STICK_BUTTON, xbox.getA().isTriggered());
 				
 		//Maps the left bumper to the grabber toggle button of the right joystick
 		rightStick.setButton(GRABBER_TOGGLE_BUTTON, xbox.getLeftBumper().isTriggered());
 		
-		//Maps y to the stop button of the right joystick
-		rightStick.setButton(ACCUMULATOR_STOP_BUTTON, xbox.getY().isTriggered());
-		
 		//Maps x to the accumulator expel button of the right joystick
-		rightStick.setButton(ACCUMULATOR_EXPEL_BUTTON, xbox.getX().isTriggered());
+		rightStick.setButton(ACCUMULATOR_EXPEL_BUTTON, expel);
 		
-		//Maps the left trigger to the accumulator accumulate button of the right joystick
-		rightStick.setButton(ACCUMULATOR_INTAKE_BUTTON, accumulate);
+		//Maps the left trigger to the accumulator intake button of the right joystick
+		rightStick.setButton(ACCUMULATOR_INTAKE_BUTTON, intake);
 		
-		//Maps the left joystick button to the shooter hold button of the left joystick
+		//Maps the left joystick button to the expeler hold button of the left joystick
 		leftStick.setButton(SHOOTER_ARM_HOLD_OPERATOR_STICK_BUTTON, xbox.getLeftStickPressed().isTriggered());
 		
 		//Maps the right joystick button to the breacher hold button of the right joystick
