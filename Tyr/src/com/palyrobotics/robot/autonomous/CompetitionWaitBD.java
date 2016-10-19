316bc12caf831dc95019460bf7563f94a7c7ecc6
Driver Station <nmitra@palyrobotics.com>
2016-10-19 11:23:31 -0700

    JZ101916-6 Added BD and WaitBD auto paths

---
+++ Tyr/src/com/palyrobotics/robot/autonomous/CompetitionWaitBD.java
@@ -0,0 +1,69 @@
+package com.palyrobotics.robot.autonomous;
+
+import org.strongback.command.Command;
+import org.strongback.command.CommandGroup;
+
+import com.palyrobotics.subsystem.drivetrain.DrivetrainController;
+import com.palyrobotics.subsystem.shooter.ShooterController;
+
+
+public class CompetitionWaitBD extends CommandGroup{
+	public CompetitionWaitBD(DrivetrainController drive, ShooterController shooter) {
+		sequentially(
+				new ShooterDownHold(shooter),
+				new CompetitionWaitBD.WaitFor(500),
+				new ShooterDeadHold(shooter),
+				new CompetitionWaitBD.WaitFor(9000),
+				new GenericDriveAuto(drive,false, 1500, 1.0f)
+		);
+	}
+	
+	public class ShooterDownHold extends Command {
+		final double UP_SPEED = 0.971 * 254 / 330;
+		private ShooterController shooter;
+		
+		public ShooterDownHold(ShooterController shooter) {
+			this.shooter = shooter;
+		}
+
+		@Override
+		public boolean execute() {			
+			shooter.systems.getArmMotor().setSpeed(-UP_SPEED);
+			return true;
+		}
+	}
+	
+
+	public class ShooterDeadHold extends Command {
+		private ShooterController shooter;
+		
+		public ShooterDeadHold(ShooterController shooter) {
+			this.shooter = shooter;
+		}
+
+		@Override
+		public boolean execute() {
+			shooter.systems.getArmMotor().setSpeed(0);
+			return true;
+		}
+	}
+	
+	public class WaitFor extends Command {
+		private double driveTime;
+		private double endTime;
+		
+		@Override
+		public void initialize() {
+			endTime = System.currentTimeMillis() + driveTime;
+		}
+		
+		public WaitFor(float driveTime) {
+			this.driveTime = driveTime;
+		}
+		
+		@Override
+		public boolean execute() {
+			return endTime < System.currentTimeMillis();			
+		}		
+	}
+}
